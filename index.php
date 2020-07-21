ad0c23b5e077b87b270ca2fef13bc8849fab69b8
guy fleury iteriteka <gfleury@disroot.org>
2020-07-21 22:59:42 +0200

    well indent and commente

--- index.php
+++ index.php
@@ -1,46 +1,48 @@
 <!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Devoir PHP OOP</title>
-</head>
-<body>
-<?php
-include 'Figure/Figure.php';
-include 'Figure/Rectangle.php';
-include 'Figure/Cercle.php';
-include 'Figure/Triangle.php';
-include 'Figure/Constant.php';
+    <head>
+	<meta charset="utf-8">
+	<title>Devoir PHP OOP</title>
+    </head>
+    <body>
+	<?php
+	// include file that contains class we defined
+	include 'Figure/Figure.php';
+	include 'Figure/Rectangle.php';
+	include 'Figure/Cercle.php';
+	include 'Figure/Triangle.php';
+	include 'Figure/Constant.php';
 
-use Devoir_oop_php_guy_fleury\Figure\Figure;
-use Devoir_oop_php_guy_fleury\Figure\Rectangle;
-use Devoir_oop_php_guy_fleury\Figure\Cercle;
-use Devoir_oop_php_guy_fleury\Figure\Triangle;
-use Devoir_oop_php_guy_fleury\Figure\Constant;
+	// import class symbol we use
+	use Devoir_oop_php_guy_fleury\Figure\Figure;
+	use Devoir_oop_php_guy_fleury\Figure\Rectangle;
+	use Devoir_oop_php_guy_fleury\Figure\Cercle;
+	use Devoir_oop_php_guy_fleury\Figure\Triangle;
+	use Devoir_oop_php_guy_fleury\Figure\Constant;
 
-echo '<br> Static variable<br>';
-echo Constant::$PI . '<br>';
-echo Constant::getPI() . '<br>';
+	echo '<br> Static variable<br>';
+	echo Constant::$PI . '<br>';
+	echo Constant::getPI() . '<br>';
 
-echo '<br>Figure Rectangle<br>';
-$rect = new Rectangle(3,4);
+	echo '<br>Figure Rectangle<br>';
+	$rect = new Rectangle(3,4);
 
-echo 'Surface : ' . $rect->surface() . '<br>';
-echo 'Perimetre : ' . $rect->perimetre() . '<br>';
+	echo 'Surface : ' . $rect->surface() . '<br>';
+	echo 'Perimetre : ' . $rect->perimetre() . '<br>';
 
-echo '<br>Figure Cercle<br>';
+	echo '<br>Figure Cercle<br>';
 
-$c = new Cercle(5);
+	$c = new Cercle(5);
 
-echo 'Surface : ' . $c->surface() . '<br>';
-echo 'Perimetre : ' . $c->perimetre() . '<br>';
+	echo 'Surface : ' . $c->surface() . '<br>';
+	echo 'Perimetre : ' . $c->perimetre() . '<br>';
 
-echo '<br> Figure Tringle <br>';
-$t = new Triangle(3,4,5);
+	echo '<br> Figure Tringle <br>';
+	$t = new Triangle(3,4,5);
 
-echo 'Surface :' . $t->surface() . '<br>';
-echo 'Perimetre :' . $t->perimetre() . '<br>'; 
+	echo 'Surface :' . $t->surface() . '<br>';
+	echo 'Perimetre :' . $t->perimetre() . '<br>';
 
-?>
+	?>
 
-</body>
+    </body>
